99c98dbb9ae93303c3849b55cd4f675ad28da778
Miladin Gavric <e11823826@student.tuwien.ac.at>
2023-10-05 22:59:23 +0200

    [US6 Backend] modifying base so I can put tournaments

--- backend/src/main/resources/sql/createSchema.sql
+++ backend/src/main/resources/sql/createSchema.sql
@@ -15,3 +15,48 @@ CREATE TABLE IF NOT EXISTS horse
   weight NUMERIC(7,2),
   breed_id BIGINT REFERENCES breed(id)
 );
+
+CREATE TABLE IF NOT EXISTS tournament
+(
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tournamentParticipants
+(
+    tournamentID INT,
+    horseID INT,
+    PRIMARY KEY (tournamentID, horseID),
+    FOREIGN KEY (tournamentID) REFERENCES tournament(id),
+    FOREIGN KEY (horseID) REFERENCES horse(id)
+);
+
+CREATE TABLE IF NOT EXISTS tournamentResult
+(
+    tournamentID INT PRIMARY KEY,
+    winnerHorseID INT,
+    FOREIGN KEY (tournamentID) REFERENCES tournament(id),
+    FOREIGN KEY (winnerHorseID) REFERENCES horse(id)
+);
+
+CREATE TABLE IF NOT EXISTS tournamentMatch
+(
+    matchID INT PRIMARY KEY,
+    tournamentID INT,
+    matchRound INT,
+    horse1ID INT,
+    horse2ID INT,
+    FOREIGN KEY (tournamentID) REFERENCES tournament(id),
+    FOREIGN KEY (horse1ID) REFERENCES horse(id),
+    FOREIGN KEY (horse2ID) REFERENCES horse(id)
+);
+
+CREATE TABLE IF NOT EXISTS tournamentMatchResult
+(
+    matchID INT PRIMARY KEY,
+    winnerHorseID INT,
+    FOREIGN KEY (matchID) REFERENCES tournamentMatch(matchID),
+    FOREIGN KEY (winnerHorseID) REFERENCES horse(id)
+);
